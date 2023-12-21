af37289f210ce4f91a22eb54b3977b2718df25ea
SalilLuley <salil.luley@gmail.com>
2023-12-21 23:31:08 +0000

    Created dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,24 +1,34 @@
 import "../css/NavBar.css";
 import "../css/Hero.css";
 import "../css/Work.css";
+import "../css/Contact.css";
 import Salil from "../assets/IMG_1335.png";
 
 const Dashboard = () => {
   return (
-    <div className="parent-sign-in">
-      <ul className="navbar">
-        <li>
-          <a href="`${ROUTES_ENUM.ABOUT}`">About</a>
-          <a href="/auth/google">Work</a>
-          <a href="/auth/google">Contact</a>
-        </li>
-      </ul>
+    <div>
       <div>
-        <div className="hero-backdrop">
+        <ul className="navbar">
+          <li>
+            <a href="`${ROUTES_ENUM.ABOUT}`">About</a>
+            <a href="/auth/google">Work</a>
+            <a href="/auth/google">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div>
           <div className="hero-section">
             <div className="hero-section-with-image">
               <img className="hero-section-img" src={Salil} alt="profile"></img>
-              <h2 style={{ color: "black" }}>Hi, I'm a software engineer</h2>
+              <h2
+                style={{
+                  color: "black",
+                }}
+              >
+                Salil Luley
+              </h2>
+              <h4>Software Engineer and yeah, that it.</h4>
             </div>
             <p style={{ color: "black", width: "50%" }}>
               I'm a software engineer with a passion for building products that
@@ -30,7 +40,36 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="work-backdrop">
-        <div>This is where I will put my work</div>
+        <div
+          className="work-hero"
+          style={{ width: "400px", height: "400px", backgroundColor: "gray" }}
+        ></div>
+        <div
+          className="work-hero"
+          style={{ width: "400px", height: "400px", backgroundColor: "gray" }}
+        ></div>
+        <div
+          className="work-hero"
+          style={{ width: "400px", height: "400px", backgroundColor: "gray" }}
+        ></div>
+      </div>
+      <div>
+        <div className="contact-backdrop">
+          <div className="contact-hero">
+            <h1>Lets Work Together</h1>
+            <p>
+              "Ready to elevate your online presence from '404 not found' to
+              'WOW, that's impressive'? Let's team up and make your website so
+              good, it'll be the Leonardo da Vinci of the internet. Ready for a
+              web revolution? Let's do this!
+            </p>
+          </div>
+          <div className="contact-form">
+            <input placeholder="Name"></input>
+            <input placeholder="Email"></input>
+            <input placeholder="Type your message"></input>
+          </div>
+        </div>
       </div>
     </div>
   );
